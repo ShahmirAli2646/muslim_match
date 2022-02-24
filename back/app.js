75dcc8a815c80ef3ffd1baa361b3acf02ef100cf
ShahmirAli2646 <shahmirali2646@gmail.com>
2022-02-24 12:04:35 +0500

    clean code structure updated and full partial potential matching logic added
    oijdoiwqjodwq

--- back/app.js
+++ back/app.js
@@ -6,180 +6,20 @@ require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const controller = require("./controllers/mainController")
 
 const app = express();
 
 app.use(express.json());
-// Register
-app.post("/register", async (req, res) => {
-
-    // Our register logic starts here
-    try {
-      // Get user input
-      const { first_name, last_name, email, password } = req.body;
-  
-      // Validate user input
-      if (!(email && password && first_name && last_name)) {
-        res.status(400).send("All input is required");
-      }
-  
-      // check if user already exist
-      // Validate if user exist in our database
-      const oldUser = await User.findOne({ email });
-  
-      if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-      }
-  
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-  
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      // save user token
-      user.token = token;
-  
-      // return new user
-      res.status(201).json(user);
-    } catch (err) {
-      console.log(err);
-    }
-    // Our register logic ends here
-  });
-  
-    
-    // Login
-    app.post("/login", async (req, res) => {
-
-        // Our login logic starts here
-        try {
-          // Get user input
-          const { email, password } = req.body;
-      
-          // Validate user input
-          if (!(email && password)) {
-            res.status(400).send("All input is required");
-          }
-          // Validate if user exist in our database
-          const user = await User.findOne({ email });
-      
-          if (user && (await bcrypt.compare(password, user.password))) {
-            // Create token
-            const token = jwt.sign(
-              { user_id: user._id, email },
-              process.env.TOKEN_KEY,
-              {
-                expiresIn: "2h",
-              }
-            );
-      
-            // save user token
-            user.token = token;
-      
-            // user
-            res.status(200).json(user);
-          }
-          res.status(400).send("Invalid Credentials");
-        } catch (err) {
-          console.log(err);
-        }
-        // Our register logic ends here
-      });
-      
-      app.post("/welcome", auth, (req, res) => {
+app.post("/register",controller.registerUser );
+app.post("/login", controller.loginUser );
+app.post("/welcome", auth, (req, res) => {
         res.status(200).send("Welcome 🙌 ");
       });
+app.post("/submit-profile-data", controller.submitProfile );
+app.get('/my-matches/full-matches/:userId/:page',controller.fullMatches);
+app.get('/my-matches/partial-matches/:userId/:page',controller.partialMatches );
+app.get('/my-matches/potential-matches/:userId/:page', controller.potentialMatches);
 
-      app.post("/submit-profile-data",  async (req, res) => {
-        try {
-            const {  purpose, 
-                gender,
-                dateofbirth,
-                prayers,
-                islamicInterest, user_id  } = req.body;
-                const profile = await Profile.create({
-                purpose, 
-                gender,
-                dateofbirth,
-                prayers,
-                islamicInterest,
-                user:user_id
-                  });
-                  res.status(201).json(profile);
-        }catch(err){
-            console.log(err);
-        }
-       
-      });
-      app.get('/my-matches/full-matches/:userId/:page', async (req, res) => {
-        const userId = req.params.userId;
-        var perPage = 5
-        var page = Math.max(0, req.params.page)
-        //run matching algorithm
-        const profiletomatch = await Profile.findOne({user:userId})
-       //now return profiles that match this profile fully
-     const FullMatches=await Profile.find({
-        purpose:profiletomatch.purpose, 
-        gender:profiletomatch.gender,
-        dateofbirth:profiletomatch.dateofbirth,
-        prayers:profiletomatch.prayers,
-        islamicInterest:profiletomatch.islamicInterest
-     })
-            .limit(perPage)
-            .skip(perPage * (page-1))   
-        res.status(201).json(FullMatches);
-      });
-      app.get('/my-matches/partial-matches/:userId/:page', async (req, res) => {
-        const userId = req.params.userId;
-        var perPage = 5
-        var page = Math.max(0, req.params.page)
-        //run matching algorithm
-        const profiletomatch = await Profile.findOne({user:userId})
-       //now return profiles that match this profile fully
-     const FullMatches=await Profile.find({
-        purpose:profiletomatch.purpose, 
-        gender:profiletomatch.gender,
-        dateofbirth:profiletomatch.dateofbirth,
-        prayers:profiletomatch.prayers,
-        islamicInterest:profiletomatch.islamicInterest
-     })
-            .limit(perPage)
-            .skip(perPage * (page-1))   
-        res.status(201).json(FullMatches);
-      });
-      app.get('/my-matches/potential-matches/:userId/:page', async (req, res) => {
-        const userId = req.params.userId;
-        var perPage = 5
-        var page = Math.max(0, req.params.page)
-        //run matching algorithm
-        const profiletomatch = await Profile.findOne({user:userId})
-       //now return profiles that match this profile fully
-     const FullMatches=await Profile.find({
-        purpose:profiletomatch.purpose, 
-        gender:profiletomatch.gender,
-        dateofbirth:profiletomatch.dateofbirth,
-        prayers:profiletomatch.prayers,
-        islamicInterest:profiletomatch.islamicInterest
-     })
-            .limit(perPage)
-            .skip(perPage * (page-1))   
-        res.status(201).json(FullMatches);
-      });
-
-// Logic goes here
 
 module.exports = app;
---
+++ back/controllers/mainController.js
@@ -0,0 +1,204 @@
+module.exports={
+     registerUser :async (req, res) => {
+
+        // Our register logic starts here
+        try {
+          // Get user input
+          const { first_name, last_name, email, password } = req.body;
+      
+          // Validate user input
+          if (!(email && password && first_name && last_name)) {
+            res.status(400).send("All input is required");
+          }
+      
+          // check if user already exist
+          // Validate if user exist in our database
+          const oldUser = await User.findOne({ email });
+      
+          if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+          }
+      
+          //Encrypt user password
+          encryptedPassword = await bcrypt.hash(password, 10);
+      
+          // Create user in our database
+          const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+          });
+      
+          // Create token
+          const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+              expiresIn: "2h",
+            }
+          );
+          // save user token
+          user.token = token;
+      
+          // return new user
+          res.status(201).json(user);
+        } catch (err) {
+          console.log(err);
+        }
+        // Our register logic ends here
+      } , 
+
+     loginUser: async (req, res) => {
+
+        // Our login logic starts here
+        try {
+          // Get user input
+          const { email, password } = req.body;
+      
+          // Validate user input
+          if (!(email && password)) {
+            res.status(400).send("All input is required");
+          }
+          // Validate if user exist in our database
+          const user = await User.findOne({ email });
+      
+          if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+              { user_id: user._id, email },
+              process.env.TOKEN_KEY,
+              {
+                expiresIn: "2h",
+              }
+            );
+      
+            // save user token
+            user.token = token;
+      
+            // user
+            res.status(200).json(user);
+          }
+          res.status(400).send("Invalid Credentials");
+        } catch (err) {
+          console.log(err);
+        }
+        // Our register logic ends here
+      } , 
+
+     submitProfile:  async (req, res) => {
+        try {
+            const {  purpose, 
+                gender,
+                dateofbirth,
+                prayers,
+                islamicInterest, user_id  } = req.body;
+                const profile = await Profile.create({
+                purpose, 
+                gender,
+                dateofbirth,
+                prayers,
+                islamicInterest,
+                user:user_id
+                  });
+                  res.status(201).json(profile);
+        }catch(err){
+            console.log(err);
+        }
+       
+      },
+
+     fullMatches: async (req, res) => {
+        const userId = req.params.userId;
+        var perPage = 5
+        var page = Math.max(0, req.params.page)
+        //run matching algorithm
+        const profiletomatch = await Profile.findOne({user:userId})
+       //now return profiles that match this profile fully
+     const FullMatches=await Profile.find({
+        purpose:profiletomatch.purpose, 
+        gender:profiletomatch.gender,
+        dateofbirth:profiletomatch.dateofbirth,
+        prayers:profiletomatch.prayers,
+        islamicInterest:profiletomatch.islamicInterest
+     })
+            .limit(perPage)
+            .skip(perPage * (page-1))
+
+        res.status(201).json(FullMatches);
+      },
+
+     partialMatches: async (req, res) => {
+        const userId = req.params.userId;
+        var perPage = 2
+        var page = Math.max(0, req.params.page)
+        //run matching algorithm
+        const profiletomatch = await Profile.findOne({user:userId})
+       //now return profiles that match this profile fully
+        const PartialMatches=await Profile.find({})      
+        const FinalResult =  PartialMatches.filter(function(item) {
+                
+                    var count = 0;
+                    if(item.purpose==profiletomatch.purpose){
+                        count++
+                    }
+                    if(item.gender==profiletomatch.gender){
+                        count++
+                    }
+                    if( item.dateofbirth==profiletomatch.dateofbirth){
+                        count++
+                    }
+                    if(item.prayers==profiletomatch.prayers){
+                        count++
+                    }
+                    if(item.islamicInterest==profiletomatch.islamicInterest){
+                        count++
+                    }
+                    if(count===3){
+                        return item
+                    }
+                }      
+        ) 
+        const paginate = (FinalResult, perPage, page) => {
+            return FinalResult.slice((page - 1) * perPage, page * perPage);
+          }
+       const response =  paginate(FinalResult , perPage , page)
+        res.status(201).json(response);
+      },
+     potentialMatches: async (req, res) => {
+        const userId = req.params.userId;
+        var perPage = 2
+        var page = Math.max(0, req.params.page)
+        //run matching algorithm
+        const profiletomatch = await Profile.findOne({user:userId})
+       //now return profiles that match this profile fully
+        const PartialMatches=await Profile.find({})     
+        const FinalResult =  PartialMatches.filter(function(item) {
+                    var count = 0;
+                    if(item.purpose==profiletomatch.purpose){
+                        count++
+                    }
+                    if(item.gender==profiletomatch.gender){
+                        count++
+                    }
+                    if( item.dateofbirth==profiletomatch.dateofbirth){
+                        count++
+                    }
+                    if(item.prayers==profiletomatch.prayers){
+                        count++
+                    }
+                    if(item.islamicInterest==profiletomatch.islamicInterest){
+                        count++
+                    }
+                    if(count===2){
+                        return item
+                    }
+                }      
+        )
+        const paginate = (FinalResult, perPage, page) => {
+            return FinalResult.slice((page - 1) * perPage, page * perPage);
+          }
+       const response =  paginate(FinalResult , perPage , page)
+        res.status(201).json(response);
+      }
+}
